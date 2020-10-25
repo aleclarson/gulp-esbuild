bd2f796cad37ef443db0f712468157be0592a373
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-10-25 18:12:28 -0400

    chore: use prettier

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
-const {Transform} = require('stream')
-const {build} = require('esbuild')
+const { Transform } = require('stream')
+const { build } = require('esbuild')
 const PluginError = require('plugin-error')
 const Vinyl = require('vinyl')
 
 const PLUGIN_NAME = 'gulp-esbuild'
 
-module.exports = function(options = {}) {
-	const entryPoints = options.entryPoints || []
+module.exports = function (options = {}) {
+  const entryPoints = options.entryPoints || []
 
-	return new Transform({
-		objectMode: true,
-		transform(file, _, cb) {
-			if (options.entryPoints) {
-				return cb(null)
-			}
-			if (file.isBuffer() || file.isStream()) {
-				entryPoints.push(file.path)
-			}
-			cb(null)
-		},
-		async flush(cb) {
-			const params = {
-				...options,
-				entryPoints,
-				write: false,
-			}
-			let data
+  return new Transform({
+    objectMode: true,
+    transform(file, _, cb) {
+      if (options.entryPoints) {
+        return cb(null)
+      }
+      if (file.isBuffer() || file.isStream()) {
+        entryPoints.push(file.path)
+      }
+      cb(null)
+    },
+    async flush(cb) {
+      try {
+        var data = await build({
+          ...options,
+          entryPoints,
+          write: false,
+        })
+      } catch (err) {
+        return cb(new PluginError(PLUGIN_NAME, err))
+      }
 
-			try {
-				data = await build(params)
-			} catch(err) {
-				return cb(new PluginError(PLUGIN_NAME, err))
-			}
+      data.outputFiles.forEach(file =>
+        this.push(
+          new Vinyl({
+            path: file.path,
+            contents: Buffer.from(file.contents),
+          })
+        )
+      )
 
-			const {outputFiles} = data
-
-			outputFiles.forEach(file =>
-				this.push(
-					new Vinyl({
-						path: file.path,
-						contents: Buffer.from(file.contents),
-					})
-				)
-			)
-
-			cb(null)
-		},
-	})
+      cb(null)
+    },
+  })
 }

4dd1394c8f75189e9158277ea29127f4dc652de5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-10-26 09:46:55 -0400

    fix(types): remove bundling-related options

--- index.d.ts
+++ index.d.ts
@@ -4,54 +4,29 @@ import * as stream from 'stream'
 
 type Format = 'iife' | 'cjs' | 'esm'
 type Strict = 'nullish-coalescing' | 'optional-chaining' | 'class-fields'
-type LogLevel = 'info' | 'warning' | 'error' | 'silent'
 type Platform = 'browser' | 'node'
-type Loader = 'js' | 'jsx' | 'ts' | 'tsx' | 'css' | 'json' | 'text' | 'base64' | 'file' | 'dataurl' | 'binary'
 type Charset = 'ascii' | 'utf8'
 
 interface CommonOptions {
-	sourcemap?: boolean | 'inline' | 'external'
-	format?: Format
-	globalName?: string
-	target?: string | string[]
-	strict?: boolean | Strict[]
+  target?: string | string[]
+  format?: Format
+  sourcemap?: boolean | 'inline' | 'external'
+  strict?: boolean | Strict[]
 
-	minify?: boolean
-	minifyWhitespace?: boolean
-	minifyIdentifiers?: boolean
-	minifySyntax?: boolean
-	charset?: Charset
+  minify?: boolean
+  minifyWhitespace?: boolean
+  minifyIdentifiers?: boolean
+  minifySyntax?: boolean
+  charset?: Charset
 
-	jsxFactory?: string
-	jsxFragment?: string
-	define?: {
-		[key: string]: string
-	}
-	pure?: string[]
-	avoidTDZ?: boolean
-
-	color?: boolean
-	logLevel?: LogLevel
-	errorLimit?: number
+  jsxFactory?: string
+  jsxFragment?: string
 }
 
 interface BuildOptions extends CommonOptions {
-	bundle?: boolean
-	splitting?: boolean
-	metafile?: string
-	platform?: Platform
-	external?: string[]
-	loader?: {
-		[ext: string]: Loader
-	}
-	resolveExtensions?: string[]
-	mainFields?: string[]
-	tsconfig?: string
-	outExtension?: {
-		[ext: string]: string
-	}
-	publicPath?: string
-	inject?: string[]
+  platform?: Platform
+  tsconfig?: string
+  inject?: string[]
 }
 
 declare const gulpEsbuild: (options: BuildOptions) => stream.Transform
